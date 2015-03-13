d5a565f8299ef9aaa966956075a2727a2e6a03d9
DamonChow <zjw10wei321@163.com>
2015-03-13 17:13:40 +0800

    测试

--- src/main/java/com/cxfdemo/ws/service/HelloRest.java
+++ src/main/java/com/cxfdemo/ws/service/HelloRest.java
@@ -1,5 +1,11 @@
 package com.cxfdemo.ws.service;
 
+import com.cxfdemo.ws.service.model.User;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
 /**
  * 功能：
  *
@@ -7,5 +13,35 @@ package com.cxfdemo.ws.service;
  */
 public interface HelloRest {
 
+    @GET
+    @Path("/getAllUsers")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<User> getAllUsers();
 
+    @GET
+    @Path("/getUser/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User getUser(@PathParam("id")String id);
+
+    @GET
+    @Path("/getUser2/{id}")
+    @Produces(MediaType.APPLICATION_XML)
+    public User getUser2(@PathParam("id")String id);
+
+    @POST
+    @Path("/saveUser")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Boolean saveUser(User u);
+
+    @PUT
+    @Path("/updateUser/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Boolean updateUser(@PathParam("id")String id, User user);
+
+    @DELETE
+    @Path("/updateUser/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Boolean deleteUser(@PathParam("id")String id);
 }
---
+++ src/main/java/com/cxfdemo/ws/service/HelloRestService.java
@@ -0,0 +1,57 @@
+package com.cxfdemo.ws.service;
+
+import com.cxfdemo.ws.service.model.User;
+
+import java.util.*;
+
+/**
+ * 功能：
+ *
+ * Created by ZhouJW on 2015/3/12 17:32.
+ */
+public class HelloRestService implements HelloRest {
+
+    private Map<String, User> map = new HashMap<String, User>();
+
+    {
+    map.put("1",new User("1","liYi",1));
+    map.put("2",new User("2","wangEr",2));
+    map.put("3",new User("3","zhangSan",1));
+
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        Collection<User> values = map.values();
+        List<User> list = new ArrayList<User>(values);
+        return list;
+    }
+
+    @Override
+    public User getUser(String id) {
+        return map.get(id);
+    }
+
+    @Override
+    public User getUser2(String id) {
+        return map.get(id);
+    }
+
+    @Override
+    public Boolean saveUser(User u) {
+        map.put(u.getId(),u);
+        return true;
+    }
+
+    @Override
+    public Boolean updateUser(String id, User u) {
+        map.put(id,u);
+        return true;
+    }
+
+    @Override
+    public Boolean deleteUser(String id) {
+        map.remove(id);
+        return true;
+    }
+}
